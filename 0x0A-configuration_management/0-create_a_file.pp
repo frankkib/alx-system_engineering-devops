727d3478274529c270137866621b8025e1745b48
frankkib <frankkibagendi13@gmail.com>
2023-04-24 09:21:05 +0000

    using puppet to create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,9 @@
 # creating a file
-$filename = '/tmp'
-file { $filename:
-  ensure => 'directory'
-}
-
 file { '/tmp/school':
   ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I Love Puppet',
+  require => File['tmp'],
 }
