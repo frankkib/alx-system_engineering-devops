899dffee5e37576352b677098708f7078cd17bac
frankkib <frankkibagendi13@gmail.com>
2023-04-25 11:23:33 +0000

    Puppet file installing and riderecting the server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,11 +4,13 @@ package { 'nginx':
   ensure => 'installed',
 }
 file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
   content => 'Hello World!',
 }
 file_path { '/etc/nginx/sites-available/default':
-  after => 'listen 80 default_server;',
-  line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+  ensure  => 'present',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   notify  => Service['nginx'],
 }
 service { 'nginx':
