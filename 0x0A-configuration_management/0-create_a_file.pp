a302c05224129c3554deaf34eb901b587475cb50
frankkib <frankkibagendi13@gmail.com>
2023-04-26 09:46:42 +0000

    fixed the path errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,11 @@
 # creating a file
+file { '/tmp':
+  require => File['/tmp/school'],
+}
 file { '/tmp/school':
   ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I Love Puppet',
-  require => File['tmp'],
 }
