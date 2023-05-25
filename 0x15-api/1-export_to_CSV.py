2a65c583ddf9baa221b8ca881a1326ff8c09a250
frankkib <frankkibagendi13@gmail.com>
2023-05-25 10:32:38 +0000

    task #0, extend your Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""This script retrieves task data for a given user
-from a public API and exports the data in CSV format.
 """
+This script retrieves task data for a given user from a public API
+"""
+
 import csv
 import requests
 import sys
 
 
 def main():
-    """A script that returns employee id"""
+    """
+    Main function that retrieves and exports task data in CSV format.
+    """
+
     if len(sys.argv) < 2:
         print("Please provide the user ID as an argument.")
         return
@@ -17,8 +21,8 @@ def main():
     main_url = 'https://jsonplaceholder.typicode.com'
 
     try:
-        todo_url = f"{main_url}/users/{user_id}/todos"
-        name_url = f"{main_url}/users/{user_id}"
+        todo_url = "{}/users/{}/todos".format(main_url, user_id)
+        name_url = "{}/users/{}".format(main_url, user_id)
 
         todo_response = requests.get(todo_url)
         name_response = requests.get(name_url)
@@ -28,20 +32,17 @@ def main():
 
         name = name_result.get("name")
 
-        filename = f"{user_id}.csv"
+        filename = "{}.csv".fomat(user_id)
         with open(filename, mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(
-                    ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
             for todo in todo_result:
-                task_completed_status = "Completed"
-                if todo["completed"] else "Not Completed"
+                task_completed_status = "Completed" if todo["completed"] else "Not Completed"
                 task_title = todo["title"]
-                writer.writerow(
-                        [user_id, name, task_completed_status, task_title])
+                writer.writerow([user_id, name, task_completed_status, task_title])
 
-        print(f"Task data exported to {filename} successfully.")
+        print("Task data exported to {} successfully.".format(filename))
 
     except requests.exceptions.RequestException as e:
         print("An error occurred: {}".format(e))
