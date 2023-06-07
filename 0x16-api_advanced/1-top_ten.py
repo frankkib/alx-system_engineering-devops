7d7073e0088ef41942e3258744bc627b81cdaa97
frankkib <frankkibagendi13@gmail.com>
2023-06-07 09:27:30 +0000

    function that finds top ten hot posts and prints them

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,17 +3,16 @@ import json
 import requests
 """module that finds the number of hot posts"""
 
+
 def top_ten(subreddit):
     """class for finding the number of hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Custom User Agent"}
-    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             posts = data["data"]["children"]
-            
             if len(posts) > 0:
                 for post in posts:
                     title = post["data"]["title"]
@@ -21,10 +20,8 @@ def top_ten(subreddit):
             else:
                 print("No posts found.")
         elif response.status_code == 302:
-
             print("None")
         else:
-
             print("Error: {}".format(response.status_code))
     except requests.exceptions.RequestException as e:
         print("Error: {}".format(e))
