19bd4554f7587d9095a19541736d41ecc1ed7977
frankkib <frankkibagendi13@gmail.com>
2023-06-08 09:23:57 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddi

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module for lsit of hot articles
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Recursively queries the Reddit API and returns a
+    list of titles of all hot articles for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+    children = data["data"]["children"]
+
+    for child in children:
+        title = child["data"]["title"]
+        hot_list.append(title)
+
+    after = data["data"]["after"]
+    if after is not None:
+        return recurse(subreddit, hot_list=hot_list)
+    else:
+        return hot_list
