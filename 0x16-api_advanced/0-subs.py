a307d0712924a8cf14792be41e864b823c8e9a66
frankkib <frankkibagendi13@gmail.com>
2023-06-12 07:38:54 +0000

    fixed documantion error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,10 +14,7 @@ def number_of_subscribers(subreddit):
             data = response.json()
             subscribers = data["data"]["subscribers"]
             return subscribers
-        elif response.status_code == 302:
-            return 0
-        else:
-            print(f"Error: {response.status_code}")
+        return 0
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
     return 0
